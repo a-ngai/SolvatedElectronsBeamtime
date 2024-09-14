bcefc27e981782f35692df53fa62c95853fe4329
Aaron Ngai (Work) <ngai.aaron@gmail.com>
2024-09-14 16:10:06 +0200

    Re-build ipynb files with Windows batch file
    Forgot to re-build the Jupyter notebooks, so they showed errors. This is
    now fixed.

---
+++ convert_to_ipynb.bat
@@ -0,0 +1,6 @@
+ipynb-py-convert examples/basic_tutorial.py examples/basic_tutorial.ipynb & 
+ipynb-py-convert examples/save_gdata.py examples/save_gdata.ipynb & 
+ipynb-py-convert examples/basic_tof_calibration.py examples/basic_tof_calibration.ipynb & 
+ipynb-py-convert examples/basic_filtering.py examples/basic_filtering.ipynb &
+ipynb-py-convert examples/basic_abel_inversion.py examples/basic_abel_inversion.ipynb
+ipynb-py-convert examples/basic_delay_analysis.py examples/basic_delay_analysis.ipynb & 
